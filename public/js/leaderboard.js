3ab846389996b1abc849a48e80ea566d0d05e788
vivian young <vivian.young.2000@gmail.com>
2021-12-02 14:03:50 -0500

    force numerical leaderboard sorting and add scrollbar

--- public/js/leaderboard.js
+++ public/js/leaderboard.js
@@ -1,10 +1,10 @@
 $(document).ready(() => {
     console.log('jquery connected');
 
-    // increment rank num
-    $( ".leaderboard-rank" ).each(function( index ) {
-        $(this).text( parseInt($(this).text()) + 1);
-    });
+    // // increment rank num
+    // $( ".leaderboard-rank" ).each(function( index ) {
+    //     $(this).text( parseInt($(this).text()) + 1);
+    // });
 
     $("#save-score-btn").click(() => {
         console.log('saving score');
@@ -18,5 +18,25 @@ $(document).ready(() => {
                 alert("score saved");
             }
         });
+    });
+
+    // force numerical sorting
+
+    let listings = Array.from($('.leaderboard-listing'));
+    $('.leaderboard-listing').remove();
+
+    listings.sort( (a,b) => {
+        let aScore = parseInt($(a).find('.leaderboard-score').text());
+        let bScore = parseInt($(b).find('.leaderboard-score').text());
+
+        if (aScore == bScore) return 0;
+        else if (aScore < bScore) return 1;
+        else return -1;
+    });
+
+    listings.forEach( (listing, i) => {
+        console.log(listing);
+        $(listing).find('.leaderboard-rank').text(i + 1);
+        $('#leaderboard').append(listing);
     });
 })
